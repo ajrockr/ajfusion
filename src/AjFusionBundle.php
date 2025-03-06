d3647c58b2d21ace95c40509d845b1c8e13b7614
arizzo <anjorizz@gmail.com>
2025-03-06 14:22:22 -0500

    added user orm xml

--- src/AjFusionBundle.php
+++ src/AjFusionBundle.php
@@ -2,9 +2,24 @@
 
 namespace Ajrockr\Ajfusion;
 
+use Doctrine\Bundle\DoctrineBundle\DependencyInjection\Compiler\DoctrineOrmMappingsPass;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\HttpKernel\Bundle\AbstractBundle;
 
 class AjFusionBundle extends AbstractBundle
 {
+    public function build(ContainerBuilder $container)
+    {
+        parent::build($container);
 
+        $userMappings = [
+            realpath(__DIR__ . '/../config/doctrine/User') => 'Ajfusion\User\Entity',
+        ];
+
+        $bundles = $container->getParameter('kernel.bundles');
+
+        if (isset($bundles['DoctrineBundle'])) {
+            $container->addCompilerPass(DoctrineOrmMappingsPass::createXmlMappingDriver($userMappings, enabledParameter: 'ajfusion_user_enabled'));
+        }
+    }
 }
